0ba02979a25831b2af852b7ef6482a3d9e8cb106
1DemonsDad1 <110293792+1DemonsDad1@users.noreply.github.com>
2023-10-05 14:40:06 +0200

    Update Untitled-1.py

--- chess/Untitled-1.py
+++ chess/Untitled-1.py
@@ -4,3 +4,4 @@
 #enpassant works
 #promotion works
 #after eating the piece protecting the king and king in check king cant eat even tho it isnt protected
+#fix list in get king rays pawn and countpwn and king moves

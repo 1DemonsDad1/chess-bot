731ebb3836265c20da1d7e3b97a851be6be5c4d2
1DemonsDad1 <110293792+1DemonsDad1@users.noreply.github.com>
2023-10-05 21:17:07 +0200

    Delete chess directory

--- chess/Untitled-1.py
+++
@@ -1,7 +0,0 @@
-#forks work
-#antivirus works
-#rosada works
-#enpassant works
-#promotion works
-#after eating the piece protecting the king and king in check king cant eat even tho it isnt protected
-#fix list in get king rays pawn and countpwn and king moves
